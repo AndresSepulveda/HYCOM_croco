1d5ec2f359a800d7911725bb0974de31bac620a5
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2024-06-06 18:17:50 -0400

    Update get_all_hycom.bash

--- get_all_hycom.bash
+++ get_all_hycom.bash
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # 10900 11651
-for i in {10900..19051}
+for i in {10910..11651}
 do
 	ncks -d time,$i,$i  -d lon,240.0,246.0 -d lat,8.0,12.0 'http://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0' all$i.nc
+	echo $i
+	sleep 1
 done
+
+
+#for i in {10900..11651}
+for i in {10910..10909}
+do
+	ncks --mk_rec_dmn time all$i.nc new$i.nc	
+	echo $i
+%	sleep 1
+done
+
+ncrcat new?????.nc all_HYCOM.nc
+
+rm all?????.nc
